ba206086d0f5b95fdccdf89a53e3bed58880a267
Shajidul Hoque Galib <shajidul.kafco@gmail.com>
2026-07-31 14:45:17 +0600

    Time: 0 ms (100.00%) | Memory: 15.1 MB (69.05%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target)
-                return i;
+        int l = 0, r = nums.size() - 1;
+        int m;
+        while (l <= r) {
+            m = l + (r - l) / 2;
+
+            if (nums[m] == target)
+                return m;
+
+            if (nums[l] <= nums[m]) {
+                if (target <= nums[m] && target >= nums[l])
+                    r = m - 1;
+
+                else
+                    l = m + 1;
+            }
+
+            else {
+                if (target >= nums[m] && target <= nums[r])
+                    l = m + 1;
+
+                else
+                    r = m - 1;
+            }
         }
+
         return -1;
     }
 };
