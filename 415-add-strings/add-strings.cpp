e9fc0549c1353689b392eda0c62ddd44cf6a0a60
Shajidul Hoque Galib <shajidul.kafco@gmail.com>
2026-08-10 01:58:44 +0600

    Time: 0 ms (100.00%) | Memory: 9 MB (95.68%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+
+        int carry = 0;
+        string ans = "";
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int a = 0;
+            int b = 0;
+
+            if (i >= 0)
+                a = num1[i] - '0';
+
+            if (j >= 0)
+                b = num2[j] - '0';
+
+            int sum = a + b + carry;
+            ans += (sum % 10) + '0';
+            carry = sum / 10;
+
+            i--;
+            j--;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
